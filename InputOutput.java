2ba1e4d3148e4a55ca0410e65de81528f1acaa9e
zhr4t <zhraanns@gmail.com>
2024-04-18 16:10:43 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,8 @@ public class InputOutput{
  public static void main(String [] args){
    Scanner scanner = new Scanner(System.in);
    String nama;
-System.out.print{"Masukkan nama anda: ");
-nama = scanner.nextLine();
-
+   System.out.print{"Masukkan nama anda: ");
+   nama = scanner.nextLine();
+   System.out.println("Hai, "+nama);
  }
 }
